f2625875ce2d5d34c2896507054e394bd62c0fd4
lucabonali <lucabonali12@gmail.com>
2017-06-15 12:00:38 +0200

    Update CreditAlert.java

--- src/client/main_client/GUI/game_mode_selection/CreditAlert.java
+++ src/client/main_client/GUI/game_mode_selection/CreditAlert.java
@@ -4,6 +4,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.scene.text.Font;
+
+import java.awt.*;
 
 /**
  * @author Andrea
@@ -25,7 +32,16 @@ class CreditAlert extends Alert {
         pane.getStylesheets().add(getClass().getResource("res/style.css").toExternalForm());
         pane.setId("privilegeAlert");
         pane.setPrefSize(WIDTH, HEGHT);
+        Text text1 = new Text("AUTHORS : (In alphabetic order, no discrimination ;) ");
+        Text text2 = new Text("BONALI LUCA");
+        Text text3 = new Text("LAMPARELLI ANDREA");
+        Text text4 = new Text("And that' s it, there's no other Credits.");
+        text1.setFont(Font.font("sans-serif",FontWeight.BOLD,11));
+        text2.setFont(Font.font("sans-serif",FontWeight.BOLD,11));
+        text3.setFont(Font.font("sans-serif",FontWeight.BOLD,11));
+        text4.setFont(Font.font("sans-serif",FontWeight.BOLD,11));
 
+        getDialogPane().setContent(new VBox(text1,text2,text3,text4));
         getButtonTypes().setAll(okButton);
         showAndWait().ifPresent(response -> {
                if (response == okButton) {
